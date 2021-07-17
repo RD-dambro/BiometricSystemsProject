8b75b8164f0ccb627bfd568ca3a546c79a415c94
RD-dambro <dambrosio.alessandro@outlook.com>
2021-07-17 15:40:21 +0200

    setup script

---
+++ bsp_setup.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/RD-dambro/BiometricSystemsClient.git client
+
+git clone https://github.com/RD-dambro/BiometricSystemsWeb.git server/web
+
+git clone https://github.com/RD-dambro/BiometricSystemsWorker.git server/worker
+
+git clone https://github.com/RD-dambro/BiometricSystemsApp.git angapp
