e0755c140fc3ca89dcd9195b12af0628f3775db4
RD-dambro <dambrosio.alessandro@outlook.com>
2021-07-17 19:11:53 +0200

    media server now uses npm version

--- bsp_setup.sh
+++ bsp_setup.sh
@@ -1,5 +1,7 @@
 git clone https://github.com/RD-dambro/BiometricSystemsClient.git client
 
+git clone https://github.com/RD-dambro/BiometricSystemsMedia.git server/media
+
 git clone https://github.com/RD-dambro/BiometricSystemsWeb.git server/web
 
 git clone https://github.com/RD-dambro/BiometricSystemsWorker.git server/worker
